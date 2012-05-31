73a63acdbdfd423e5bef05cbcf4d68a0bd072b73
Mathieu AVILA <mathieu.avila@laposte.net>
2012-05-31 11:46:38 +0200

    Fixed badly modified contentReport

--- src/com/TheJobCoach/webapp/userpage/client/ContentReport.java
+++ src/com/TheJobCoach/webapp/userpage/client/ContentReport.java
@@ -2,11 +2,17 @@ package com.TheJobCoach.webapp.userpage.client;
 
 import com.TheJobCoach.webapp.mainpage.shared.UserId;
 import com.TheJobCoach.webapp.userpage.client.images.ClientImageBundle;
+import com.TheJobCoach.webapp.util.client.ButtonImageText;
 import com.TheJobCoach.webapp.util.client.ContentHelper;
+import com.TheJobCoach.webapp.util.client.MessageBox;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextArea;
 
 /**
@@ -18,7 +24,7 @@ public class ContentReport implements EntryPoint {
 	private VerticalPanel simplePanelCenter = new VerticalPanel();
 	private TextArea textArea = new TextArea();
 	private Lang lang = GWT.create(Lang.class);
-	
+
 	public void setUserParameters(UserId _user)
 	{
 		user = _user;
@@ -33,23 +39,61 @@ public class ContentReport implements EntryPoint {
 		rootPanel = panel;
 	}
 
+	private void sendComment()
+	{
+		AsyncCallback<String> callback = new AsyncCallback<String>() {
+			@Override
+			public void onFailure(Throwable caught)
+			{
+				MessageBox.messageBoxException(rootPanel, caught.getMessage());
+			}
+			@Override
+			public void onSuccess(String result)
+			{
+				simplePanelCenter.clear();
+				Label thanks = new Label(lang._TextReplyComment());
+				thanks.setStyleName("standard-text");
+				simplePanelCenter.add(thanks);
+			}
+		};
+		userService.sendComment(user, textArea.getValue(), callback);
+	}
+
 	/**
 	 * This is the entry point method.
 	 * @wbp.parser.entryPoint
 	 */
 	public void onModuleLoad()
-	{			
+	{
 		// Add the nameField and sendButton to the RootPanel
 		// Use RootPanel.get() to get the entire body element
 		//RootPanel rootPanel = RootPanel.get("centercontent");
 		rootPanel.setSize("100%", "100%");
 		rootPanel.clear();
-				
+
 		simplePanelCenter.setSize("100%", "100%");
 		VerticalPanel uberPanel = new VerticalPanel();
 		rootPanel.add(uberPanel);
-		
-		ContentHelper.insertTitlePanel(uberPanel, lang._TextReport(), ClientImageBundle.INSTANCE.sendComment());
+
+		ContentHelper.insertTitlePanel(uberPanel, lang._TextMakeComment(), ClientImageBundle.INSTANCE.sendComment());
 		uberPanel.add(simplePanelCenter);
+
+		Label labelExplanation = new Label(lang._TextAboutComment());
+		labelExplanation.setStyleName("standard-text");
+		simplePanelCenter.add(labelExplanation);
+
+		simplePanelCenter.add(textArea);
+		textArea.setSize("100%", "200px");
+
+		final ButtonImageText buttonSendReport = new ButtonImageText(ButtonImageText.Type.MAIL, lang._TextSendComment());
+		buttonSendReport.addClickHandler(new ClickHandler()
+		{
+			@Override
+			public void onClick(ClickEvent event) {
+				buttonSendReport.setEnabled(false);
+				sendComment();
+			}
+		});
+		simplePanelCenter.add(buttonSendReport);
 	}
 }
