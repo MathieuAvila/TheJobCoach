d7c569615da9eb40fb28cd99c23f8299a7308f07
Mathieu AVILA <mathieu.avila@laposte.net>
2013-03-27 22:38:39 +0100

    clean-up imports

--- src/com/TheJobCoach/webapp/userpage/client/Opportunity/EditLogEntry.java
+++ src/com/TheJobCoach/webapp/userpage/client/Opportunity/EditLogEntry.java
@@ -24,13 +24,12 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.RichTextArea;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.RichTextArea;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.datepicker.client.DateBox;
 
