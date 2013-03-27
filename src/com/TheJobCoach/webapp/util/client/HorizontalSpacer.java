7953cbee9c5c0b36c3e519b6ee32371b35a52584
Mathieu AVILA <mathieu.avila@laposte.net>
2013-03-27 21:49:28 +0100

    Add spacers

---
+++ src/com/TheJobCoach/webapp/util/client/HorizontalSpacer.java
@@ -0,0 +1,12 @@
+package com.TheJobCoach.webapp.util.client;
+
+import com.google.gwt.user.client.ui.SimplePanel;
+
+public class HorizontalSpacer extends SimplePanel
+{
+	public HorizontalSpacer(String size)
+	{
+		super();
+		super.setWidth(size);		
+	}
+}
---
+++ src/com/TheJobCoach/webapp/util/client/VerticalSpacer.java
@@ -0,0 +1,12 @@
+package com.TheJobCoach.webapp.util.client;
+
+import com.google.gwt.user.client.ui.SimplePanel;
+
+public class VerticalSpacer extends SimplePanel
+{
+	public VerticalSpacer(String size)
+	{
+		super();
+		super.setHeight(size);		
+	}
+}
