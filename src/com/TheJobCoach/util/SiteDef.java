18c82c0d05694f7240a12d61012b03778310d9d0
MathieuAvila <avila@titan.(none)>
2013-04-23 06:42:51 +0200

    fix website address.... OMG.

--- src/com/TheJobCoach/util/SiteDef.java
+++ src/com/TheJobCoach/util/SiteDef.java
@@ -2,7 +2,7 @@ package com.TheJobCoach.util;
 
 public class SiteDef {
 
-	static String webAddress = "www.thejobcoach.fr:8080/TheJobCoach";
+	static String webAddress = "www.thejobcoach.fr/TheJobCoach";
 	
 	static public String getAddress()
 	{
