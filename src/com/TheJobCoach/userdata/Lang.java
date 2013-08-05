7cf776bc6fddbfb398403b82d842754a2fbf20e0
Mathieu AVILA <mathieu.avila@laposte.net>
2013-08-05 22:16:52 +0200

    Lang properties instead of functions

--- src/com/TheJobCoach/userdata/Lang.java
+++ src/com/TheJobCoach/userdata/Lang.java
@@ -42,23 +42,9 @@ public class Lang
 		return langProp.get(getLang(lang));
 	}
 	
-	static String _TextBody(String firstName, String site, String userName, String key, String lang)
+	static String _TextActivateAccountBody(String firstName, String site, String userName, String key, String lang)
 	{
-		String body="";
-		if (lang.equals("en"))
-		{
-			body="Hello {0}\n\n"+
-					"You can activate your account by clicking on the following link:\n"+
-					"{1}/TheJobCoach.html?action=validate&username={2}&token={3}\n\n" +
-					"We are happy to to see you soon on The Job Coach !\n\n"+
-					"The Job Coach team\n";
-		} else {
-			body="Bonjour {0}\n\n"+
-					"Vous pouvez activer votre compte en cliquant sur le lien suivant:\n"+
-					"{1}/TheJobCoach.html?action=validate&username={2}&token={3}\n\n"+
-					"Nous sommes heureux de vous voir sur The Job Coach !\n\n"+
-					"L'equipe The Job Coach\n";
-		}
+		String body=getLangProp(lang).getProperty("activateaccountbody");
 		body = body.replace("{0}", firstName);
 		body = body.replace("{1}", site);
 		body = body.replace("{2}", userName);
@@ -66,53 +52,21 @@ public class Lang
 		return body;
 	}
 
-	static String _TextSubject(String lang)
+	static String _TextActivateAccountSubject(String lang)
 	{
-		String subject="";
-		if (lang.equals("en"))
-		{
-			subject="Activate Account on www.TheJobCoach.fr";
-		} 
-		else
-		{
-			subject="Activer votre compte www.TheJobCoach.fr";
-		}
-		return subject;
+		return getLangProp(lang).getProperty("credentialssubject");
 	}
 	
 	static String _TextLostCredentialsSubject(String lang)
 	{
-		String subject="";
-		if (lang.equals("en"))
-		{
-			subject="Your credentials on www.TheJobCoach.fr";
-		} 
-		else
-		{
-			subject="Vos identifiants sur www.TheJobCoach.fr !";
-		}
-		return subject;
+		return getLangProp(lang).getProperty("activateaccountsubject");
 	}
 	
 	static String _TextLostCredentials(String user, String password, String lang)
 	{
-		String body="";
-		if (lang.equals("en"))
-		{
-			body=
-					"You asked for your credentials on www.TheJobCoach.fr\n"+
-					"Your user name is: '" + user + "'\n"+
-					"Your password is: '" + password + "'\n\n"+
-					"Hoping to see you soon on www.TheJobCoach.fr!";
-		} 
-		else
-		{
-			body=
-					"Vous avez demandé vos identifiants de www.TheJobCoach.fr\n"+
-					"Votre login est : '" + user + "'\n"+
-					"Votre mot de passe est : '" + password + "'\n\n"+
-					"En espérant vous revoir bientôt sur www.TheJobCoach.fr !";
-		}
+		String body=getLangProp(lang).getProperty("credentialsbody");
+		body = body.replace("{0}", user);
+		body = body.replace("{1}", password);
 		return body;
 	}
 	
