9773079269bbc83a8129137ff2ec796074784d2c
Mathieu AVILA <mathieu.avila@laposte.net>
2013-08-05 22:18:04 +0200

    Fix sitedef value !

--- src/com/TheJobCoach/util/SiteDef.java
+++ src/com/TheJobCoach/util/SiteDef.java
@@ -2,7 +2,7 @@ package com.TheJobCoach.util;
 
 public class SiteDef {
 
-	static String webAddress = "www.thejobcoach.fr/TheJobCoach";
+	static String webAddress = "http://www.thejobcoach.fr/TheJobCoach";
 	
 	static public String getAddress()
 	{
