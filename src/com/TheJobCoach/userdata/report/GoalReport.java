367a9a36ca354a979d5aba2d26e7607a3e562b68
MathieuAvila <mathieu.avila@laposte.net>
2013-11-03 22:04:19 +0100

    Goal report, a wrapper to other managers

---
+++ src/com/TheJobCoach/userdata/report/GoalReport.java
@@ -0,0 +1,53 @@
+package com.TheJobCoach.userdata.report;
+
+import java.util.Date;
+import java.util.Vector;
+
+import com.TheJobCoach.userdata.ConnectionLog;
+import com.TheJobCoach.userdata.UserLogManager;
+import com.TheJobCoach.userdata.UserOpportunityManager;
+import com.TheJobCoach.webapp.userpage.shared.GoalReportInformation;
+import com.TheJobCoach.webapp.userpage.shared.UserId;
+import com.TheJobCoach.webapp.userpage.shared.UserLogEntry;
+import com.TheJobCoach.webapp.userpage.shared.UserOpportunity;
+import com.TheJobCoach.webapp.util.shared.CassandraException;
+
+public class GoalReport
+{
+	static UserLogManager log = new UserLogManager();
+	static UserOpportunityManager oppManager = new UserOpportunityManager();
+	ConnectionLog connectionLog = new ConnectionLog();
+	
+	public GoalReportInformation getReport(UserId id, Date start, Date end) throws CassandraException 
+	{
+		GoalReportInformation result = new GoalReportInformation(start, end);
+		
+		// Get logs type on the period
+		Vector<String> logs = log.getPeriodUserOppStatusChange(id, start, end);
+		for (String logId: logs)
+		{
+			UserLogEntry realLog = log.getLogEntryLong(id, logId);
+			Integer previous = result.log.get(realLog.type);
+			Integer newValue = new Integer(previous.intValue() + 1);
+			result.log.put(realLog.type, newValue);
+		}
+		
+		// Build new opportunity counter
+		Vector<UserOpportunity> oppList = oppManager.getOpportunitiesList(id, UserOpportunityManager.MANAGED_LIST);
+		for (UserOpportunity opp: oppList)
+		{
+			if (start.before(opp.firstSeen) && (end.after(opp.firstSeen)))
+			{
+				result.newOpportunities++;
+			}
+		}
+		
+		// Time logger: number of days logged in.
+		ConnectionLog.LogTimeResult logTime = connectionLog.getLogDays(id.userName, start, end);
+		result.connectedDays = logTime.totalDay;
+		result.succeedStartDay = logTime.startOk;
+		result.succeedEndDay = logTime.endOk;
+		
+		return result;
+	}
+}
---
+++ src/com/TheJobCoach/webapp/userpage/shared/GoalReportInformation.java
@@ -0,0 +1,32 @@
+package com.TheJobCoach.webapp.userpage.shared;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GoalReportInformation implements Serializable
+{
+
+	private static final long serialVersionUID = -721348694020391393L;
+	
+	public Map<UserLogEntry.LogEntryType, Integer> log = new HashMap<UserLogEntry.LogEntryType, Integer>();
+	public int newOpportunities;
+	
+	public int connectedDays;
+	public int succeedStartDay;
+	public int succeedEndDay;
+	
+	public Date start;
+	public Date end;
+	
+	public GoalReportInformation(Date start, Date end)
+	{
+		this.start = start;
+		this.end = end;
+		for (UserLogEntry.LogEntryType type: UserLogEntry.LogEntryType.values())
+		{
+			log.put(type,  new Integer(0));
+		}
+	}
+}
