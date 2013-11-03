42f9bda746a4570f6ccce512f5a80d9f98a62692
MathieuAvila <mathieu.avila@laposte.net>
2013-11-03 22:03:53 +0100

    I18N FOR ALL

--- src/com/TheJobCoach/webapp/userpage/client/MyGoals/ContentMyGoals.java
+++ src/com/TheJobCoach/webapp/userpage/client/MyGoals/ContentMyGoals.java
@@ -51,31 +51,31 @@ public class ContentMyGoals implements EntryPoint, IChanged, ReturnValue, IApply
 	
 	CheckedExtendedDropListField tfGoalPeriod = new CheckedExtendedDropListField(
 			UserValuesConstantsMyGoals.PERFORMANCE_EVALUATION_PERIOD_LIST, langGoals.periodMap(), "periodMap_");
-	CheckedLabel clGoalPeriod = new CheckedLabel("Période d'évaluation", false, tfGoalPeriod);
+	CheckedLabel clGoalPeriod = new CheckedLabel(langGoals.evaluationPeriodSub(), false, tfGoalPeriod);
 
 	CheckedTime tfConnectBefore = new CheckedTime(CheckedTime.DAY_MIDDAY);
-	CheckedLabel clConnectBefore = new CheckedLabel("Je dois me connecter avant cette heure ", false, tfConnectBefore);
+	CheckedLabel clConnectBefore = new CheckedLabel(langGoals.imust_beforehour(), false, tfConnectBefore);
 
 	CheckedTime tfConnectAfter = new CheckedTime( CheckedTime.DAY_END);
-	CheckedLabel clConnectAfter = new CheckedLabel("Je dois être déconnecté après cette heure", false, tfConnectAfter);
+	CheckedLabel clConnectAfter = new CheckedLabel(langGoals.imustnot_afterhour(), false, tfConnectAfter);
 
 	CheckedTextField tfConnectRatio = new CheckedTextField("[0-9]*");
-	CheckedLabel clConnectRatio = new CheckedLabel("Je dois me connecter au moins ce nombre de jours", false, tfConnectRatio);
+	CheckedLabel clConnectRatio = new CheckedLabel(langGoals.imustnot_thisnumber(), false, tfConnectRatio);
 
 	CheckedTextField tfCreateOpportunity = new CheckedTextField("[0-9]*");
-	CheckedLabel clCreateOpportunity = new CheckedLabel("Je dois créer au moins ce nombre d'opportunités", false, tfCreateOpportunity);
+	CheckedLabel clCreateOpportunity = new CheckedLabel(langGoals.imust_createopp(), false, tfCreateOpportunity);
 	
 	CheckedTextField tfCandidateOpportunity = new CheckedTextField("[0-9]*");
-	CheckedLabel clCandidateOpportunity = new CheckedLabel("Je dois candidater à au moins ce nombre d'opportunités", false, tfCandidateOpportunity);
+	CheckedLabel clCandidateOpportunity = new CheckedLabel(langGoals.imust_candidate(), false, tfCandidateOpportunity);
 	
 	CheckedTextField tfInterviewOpportunity = new CheckedTextField("[0-9]*");
-	CheckedLabel clInterviewOpportunity = new CheckedLabel("Je dois passer au moins ce nombre d'entretiens", false, tfInterviewOpportunity);
+	CheckedLabel clInterviewOpportunity = new CheckedLabel(langGoals.imust_interviews(), false, tfInterviewOpportunity);
 	
 	CheckedTextField tfProposal = new CheckedTextField("[0-9]*");
-	CheckedLabel clProposal = new CheckedLabel("Je dois obtenir au moins ce nombre de propositions", false, tfProposal);
+	CheckedLabel clProposal = new CheckedLabel(langGoals.imust_proposals(), false, tfProposal);
 	
 	CheckedTextField tfPhoneCall = new CheckedTextField("[0-9]*");
-	CheckedLabel clPhoneCall = new CheckedLabel("Je dois passer ce nombre d'appels téléphoniques", false, tfPhoneCall);
+	CheckedLabel clPhoneCall = new CheckedLabel(langGoals.imust_phonecalls(), false, tfPhoneCall);
 	
 	
 	void getValues()
@@ -100,14 +100,14 @@ public class ContentMyGoals implements EntryPoint, IChanged, ReturnValue, IApply
 
 		ContentHelper.insertTitlePanel(simplePanelCenter, lang._TextMyGoals(), ClientImageBundle.INSTANCE.userVirtualCoachGoalsContent());
 
-		ContentHelper.insertSubTitlePanel(simplePanelCenter, "Ma période d'évaluation");
+		ContentHelper.insertSubTitlePanel(simplePanelCenter, langGoals.evaluationPeriod());
 		Grid gridPeriod = new Grid(1, 2);
 		simplePanelCenter.add(gridPeriod);
 		gridPeriod.setWidget(0,0, clGoalPeriod);
 		gridPeriod.setWidget(0,1, tfGoalPeriod.getItem());
 		fields.put(UserValuesConstantsMyGoals.PERFORMANCE_EVALUATION_PERIOD, tfGoalPeriod);
 
-		ContentHelper.insertSubTitlePanel(simplePanelCenter, "Mes horaires de connection");
+		ContentHelper.insertSubTitlePanel(simplePanelCenter, langGoals.connectionTimes());
 
 		Grid grid0 = new Grid(3, 2);
 		simplePanelCenter.add(grid0);
@@ -125,7 +125,7 @@ public class ContentMyGoals implements EntryPoint, IChanged, ReturnValue, IApply
 		fields.put(UserValuesConstantsMyGoals.PERFORMANCE_CONNECT_NOT_AFTER_HOUR, tfConnectAfter);
 		fields.put(UserValuesConstantsMyGoals.PERFORMANCE_CONNECT_RATIO, tfConnectRatio);
 		
-		ContentHelper.insertSubTitlePanel(simplePanelCenter, "Mes objectifs d'opportunités");
+		ContentHelper.insertSubTitlePanel(simplePanelCenter, langGoals.opportunityGoals());
 
 		Grid grid1 = new Grid(1, 2);
 		simplePanelCenter.add(grid1);
@@ -143,7 +143,7 @@ public class ContentMyGoals implements EntryPoint, IChanged, ReturnValue, IApply
 
 		fields.put(UserValuesConstantsMyGoals.PERFORMANCE_CANDIDATEOPPORTUNITY, tfCandidateOpportunity);
 
-		ContentHelper.insertSubTitlePanel(simplePanelCenter, "Mes objectifs d'entretiens");
+		ContentHelper.insertSubTitlePanel(simplePanelCenter, langGoals.interviewGoals());
 
 		Grid grid3 = new Grid(1, 2);
 		simplePanelCenter.add(grid3);
@@ -153,7 +153,7 @@ public class ContentMyGoals implements EntryPoint, IChanged, ReturnValue, IApply
 
 		fields.put(UserValuesConstantsMyGoals.PERFORMANCE_INTERVIEW, tfInterviewOpportunity);
 
-		ContentHelper.insertSubTitlePanel(simplePanelCenter, "Mes objectifs d'appels téléphoniques");
+		ContentHelper.insertSubTitlePanel(simplePanelCenter, langGoals.phonecallGoals());
 
 		Grid grid4 = new Grid(1, 2);
 		simplePanelCenter.add(grid4);
@@ -163,7 +163,7 @@ public class ContentMyGoals implements EntryPoint, IChanged, ReturnValue, IApply
 
 		fields.put(UserValuesConstantsMyGoals.PERFORMANCE_PHONECALL, tfPhoneCall);
 			
-		ContentHelper.insertSubTitlePanel(simplePanelCenter, "Mes objectifs de propositions");
+		ContentHelper.insertSubTitlePanel(simplePanelCenter, langGoals.proposalGoals());
 
 		Grid grid5 = new Grid(1, 2);
 		simplePanelCenter.add(grid5);
--- src/com/TheJobCoach/webapp/userpage/client/MyGoals/LangGoals.java
+++ src/com/TheJobCoach/webapp/userpage/client/MyGoals/LangGoals.java
@@ -9,4 +9,51 @@ public interface LangGoals extends Constants {
 	@Key("periodMap")
 	Map<String, String> periodMap();
 
+	@Key("evaluationperiod")
+	String evaluationPeriod();
+
+	@Key("connectiontimes")
+	String connectionTimes();
+
+	@Key("opportunitygoals")
+	String opportunityGoals();
+
+	@Key("interviewgoals")
+	String interviewGoals();
+
+	@Key("phonecallgoals")
+	String phonecallGoals();
+
+	@Key("proposalGoals")
+	String proposalGoals();
+
+
+
+	@Key("evaluationperiodsub")
+	String evaluationPeriodSub();
+
+
+	@Key("imust_beforehour")
+	String imust_beforehour();
+	
+	@Key("imustnot_afterhour")
+	String imustnot_afterhour();
+	
+	@Key("imustnot_thisnumber")
+	String imustnot_thisnumber();
+	
+	@Key("imust_createopp")
+	String imust_createopp();
+	
+	@Key("imust_candidate")
+	String imust_candidate();
+	
+	@Key("imust_interviews")
+	String imust_interviews();
+	
+	@Key("imust_proposals")
+	String imust_proposals();
+	
+	@Key("imust_phonecalls")
+	String imust_phonecalls();
 }
