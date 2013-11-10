1921acdcee2af14759f9c4f1928ddcb85092f085
MathieuAvila <mathieu.avila@laposte.net>
2013-11-10 09:44:33 +0100

    clean-up

--- src/com/TheJobCoach/webapp/util/shared/UserValuesConstantsMyGoals.java
+++ src/com/TheJobCoach/webapp/util/shared/UserValuesConstantsMyGoals.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.google.common.collect.ImmutableMap;
-
 public class UserValuesConstantsMyGoals 
 {	
 	public final static String PERFORMANCE_EVALUATION_PERIOD        = "PERFORMANCE_EVALUATION_PERIOD";
