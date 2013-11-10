50dc6a307abe1c2bec893d52b5ef2d1d32ba4e92
MathieuAvila <mathieu.avila@laposte.net>
2013-11-10 11:21:18 +0100

    move images to subdir

--- src/com/TheJobCoach/webapp/util/client/ClientImageBundle.java
+++ src/com/TheJobCoach/webapp/util/client/ClientImageBundle.java
@@ -14,51 +14,51 @@ public interface ClientImageBundle extends ClientBundle
 	 */
 	//public ClippedImagePrototype deleteFile();	
 	
-	@Source("button_cancel_24.png")
+	@Source("images/button_cancel_24.png")
 	ImageResource createCancel();
 
-	@Source("button_ok_24.gif")
+	@Source("images/button_ok_24.gif")
 	ImageResource createOk();
 	
-	@Source("new_24.png")
+	@Source("images/new_24.png")
 	ImageResource createNew();
 
-	@Source("button_add_24.png")
+	@Source("images/button_add_24.png")
 	ImageResource buttonAdd24();
 
-	@Source("button_add_16.png")
+	@Source("images/button_add_16.png")
 	ImageResource buttonAdd16();
 
-	@Source("question_64.png")
+	@Source("images/question_64.png")
 	ImageResource createQuestion();
 
-	@Source("info_64.png")
+	@Source("images/info_64.png")
 	ImageResource createInfo();
 
-	@Source("error_64.png")
+	@Source("images/error_64.png")
 	ImageResource createError();
 
-	@Source("warning_64.png")
+	@Source("images/warning_64.png")
 	ImageResource createWarning();
 
-	@Source("wait_64.png")
+	@Source("images/wait_64.png")
 	ImageResource createWait();
 
-	@Source("left_24.png")
+	@Source("images/left_24.png")
 	ImageResource backIcon();
 
-	@Source("right_24.png")
+	@Source("images/right_24.png")
 	ImageResource nextIcon();
 
-	@Source("email_32.png")
+	@Source("images/email_32.png")
 	ImageResource emailIcon();
 
-	@Source("failure_24.png")
+	@Source("images/failure_24.png")
 	ImageResource failure_24();
 
-	@Source("success_24.png")
+	@Source("images/success_24.png")
 	ImageResource success_24();
 
-	@Source("unknown_24.png")
+	@Source("images/unknown_24.png")
 	ImageResource unknown_24();
 }
