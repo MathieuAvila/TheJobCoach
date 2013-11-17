3c14031abe152d91e4665dd0c38f53c5482effad
MathieuAvila <mathieu.avila@laposte.net>
2013-11-17 15:53:36 +0100

    deploy to tomcat v6 or v7

--- deploy_war_local.sh
+++ deploy_war_local.sh
@@ -3,4 +3,12 @@ cd war
 rm -f TheJobCoach.war
 jar -cvf TheJobCoach.war .
 chmod a+r TheJobCoach.war
-mv TheJobCoach.war /var/lib/tomcat6/webapps/
+if [ -d /var/lib/tomcat6/webapps ]
+then
+  mv TheJobCoach.war /var/lib/tomcat6/webapps/
+elif [ -d /var/lib/tomcat7/webapps ]
+  then
+  mv TheJobCoach.war /var/lib/tomcat7/webapps/
+  else
+  echo "Unable to find tomcat directory"
+fi
