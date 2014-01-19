48b18d95ff4c6d62d7cf42472766e4fc0b26837c
MathieuAvila <mathieu.avila@laposte.net>
2014-01-19 22:37:06 +0100

    uodate user site when clicking on link

--- src/com/TheJobCoach/webapp/userpage/client/UserSite/ContentUserSite.java
+++ src/com/TheJobCoach/webapp/userpage/client/UserSite/ContentUserSite.java
@@ -23,8 +23,15 @@ import com.TheJobCoach.webapp.util.shared.UserId;
 import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.DomEvent;
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -35,7 +42,9 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 public class ContentUserSite implements EntryPoint {
 
 	final Lang lang = GWT.create(Lang.class);
-
+	
+	VerticalPanel simplePanelCenter = new VerticalPanel();
+	
 	UserId user;
 
 	// The list of data to display.
@@ -117,7 +126,6 @@ public class ContentUserSite implements EntryPoint {
 		rootPanel.setSize("100%", "100%");
 		rootPanel.clear();
 
-		VerticalPanel simplePanelCenter = new VerticalPanel();
 		simplePanelCenter.setSize("100%", "");
 		rootPanel.add(simplePanelCenter);
 
@@ -127,22 +135,41 @@ public class ContentUserSite implements EntryPoint {
 			@Override
 			public void update(int index, UserJobSite object, String value) {
 				deleteSite(object);
-			}}	
-				);
+			}});
 
 		cellTable.addColumnWithIcon(IconCellSingle.IconType.UPDATE, new FieldUpdater<UserJobSite, String>() {
 			@Override
 			public void update(int index, UserJobSite object, String value) {
 				updateSite(object);
-			}}	
-				);
+			}});
 
 		// Create URL column.
+		cellTable.addColumnWithIcon(IconCellSingle.IconType.URL, new FieldUpdater<UserJobSite, String>() {
+			@Override
+			public void update(int index, final UserJobSite object, String value) {
+				object.update.last = new Date();
+				
+				final Anchor link = new Anchor("", object.URL);
+				simplePanelCenter.add(link);
+				link.setTarget("_blank");
+				clickElement(link.getElement());
+				
+				userService.setUserSite(user, object, new ServerCallHelper<Integer>(rootPanel)
+						{	
+					@Override
+					public void onSuccess(Integer r)
+					{
+						cellTable.redraw();
+					}
+				});
+			}});
+
+		/*
 		cellTable.addColumnUrl(new ExtendedCellTable.GetValue<String, UserJobSite>() {
 			public String getValue(UserJobSite contact) {
 				return contact.URL;
 			}
-		});
+		});*/
 
 		// Create name column.
 		cellTable.specialAddColumnSortableString(new GetValue<String, UserJobSite>() {
@@ -198,4 +225,10 @@ public class ContentUserSite implements EntryPoint {
 		});
 		simplePanelCenter.add(buttonNewSite);
 	}
+	
+	/* Helper to simulate JS click */
+	public static native void clickElement(Element elem) /*-{
+    elem.click();
+	}-*/;
+
 }
