b41c00369a70f562750c0ab377514242fbeff45f
MathieuAvila <mathieu.avila@laposte.net>
2014-05-15 09:28:28 +0200

    add searchresult to the account interface

--- src/com/TheJobCoach/userdata/AccountInterface.java
+++ src/com/TheJobCoach/userdata/AccountInterface.java
@@ -2,11 +2,13 @@ package com.TheJobCoach.userdata;
 
 import java.util.Vector;
 
+import com.TheJobCoach.webapp.adminpage.shared.UserSearchResult;
 import com.TheJobCoach.webapp.mainpage.shared.MainPageReturnCode.CreateAccountStatus;
 import com.TheJobCoach.webapp.mainpage.shared.MainPageReturnCode.ValidateAccountStatus;
 import com.TheJobCoach.webapp.mainpage.shared.MainPageReturnLogin;
 import com.TheJobCoach.webapp.mainpage.shared.UserInformation;
 import com.TheJobCoach.webapp.util.shared.CassandraException;
+import com.TheJobCoach.webapp.util.shared.SystemException;
 import com.TheJobCoach.webapp.util.shared.UserId;
 
 public interface AccountInterface
@@ -26,4 +28,8 @@ public interface AccountInterface
 	public void purgeAccount() throws CassandraException;
 
 	public void setPassword(UserId id, String newPassword) throws CassandraException;
+
+	public UserSearchResult searchUsers(UserId id, String firstName,
+			String lastName, int sizeRange, int startRange) throws CassandraException, SystemException
+;
 }
