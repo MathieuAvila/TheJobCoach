e2252c48e975cb0918c5306124e79e1940b5446a
MathieuAvila <mathieu.avila@laposte.net>
2014-05-20 23:01:50 +0200

    message box tri state

---
+++ src/com/TheJobCoach/webapp/util/client/MessageBoxTriState.java
@@ -0,0 +1,176 @@
+package com.TheJobCoach.webapp.util.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Button;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class MessageBoxTriState implements EntryPoint {
+
+	Panel rootPanel;
+	
+	static final LangUtil lang = GWT.create(LangUtil.class);
+
+	public enum TYPE { WARNING, QUESTION };
+	public interface ICallback
+	{
+		public void complete(int choice);
+	}
+
+	boolean ok = true;
+	boolean cancel = true;
+	TYPE type = TYPE.QUESTION;
+	String title;
+	String message;
+	ICallback callback;
+	Button button1;
+	Button button2;
+	Button buttonCancel;
+	
+	public MessageBoxTriState(Panel rootPanel, TYPE type, String title, String message, Button button1, Button button2, ICallback callback)
+	{
+		this.rootPanel = rootPanel;
+		this.button1 = button1;
+		this.button2 = button2;
+		this.type = type;
+		this.title = title;
+		this.message = message;
+		this.callback = callback;
+	}
+
+	static ClientImageBundle wpImageBundle = (ClientImageBundle) GWT.create(ClientImageBundle.class);
+	static ImageResource infoIcon = wpImageBundle.createInfo();
+	static ImageResource warningIcon = wpImageBundle.createWarning();
+	static ImageResource questionIcon = wpImageBundle.createQuestion();
+	static ImageResource errorIcon = wpImageBundle.createError();
+	static ImageResource waitIcon = wpImageBundle.createWait();
+
+	final DialogBox dBox = new DialogBox();
+
+	public void close()
+	{
+		dBox.hide();
+	}
+
+	/* For UT purposes only. 
+	 * This is used to redirect errors to a specific handler. 
+	 * It shouldn't be used in production code. */
+	public interface ErrorCatcher
+	{
+		public void errorEvent(MessageBoxTriState error, TYPE type, String title, String message);
+	}	
+	static private ErrorCatcher currentErrorCatcher = null;
+	static public void registerErrorCatcher(ErrorCatcher catcher)	
+	{
+		currentErrorCatcher = catcher;
+	}
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad()
+	{
+		if (currentErrorCatcher != null)
+		{
+			currentErrorCatcher.errorEvent(this, type, title, message);
+		}
+		
+		dBox.setText(title);
+		dBox.setGlassEnabled(true);
+		dBox.setAnimationEnabled(true);
+		dBox.setModal(true);
+        
+		FlexTable flexTable = new FlexTable();
+		dBox.setWidget(flexTable);
+		flexTable.setSize("100%", "100%");
+
+		HorizontalPanel horizontalPanelSpaceUp = new HorizontalPanel();
+		horizontalPanelSpaceUp.setHeight("10px");
+		horizontalPanelSpaceUp.setWidth("100px");
+		flexTable.setWidget(0, 0, horizontalPanelSpaceUp);
+
+		Image imageIcon = null;
+		switch (type)
+		{
+		case WARNING:  imageIcon = new Image(warningIcon); break;
+		case QUESTION: imageIcon = new Image(questionIcon); break;
+		default:
+			break;
+		}
+
+		if (imageIcon != null) 
+		{
+			flexTable.setWidget(1, 0, imageIcon);
+		}
+		else
+		{
+			horizontalPanelSpaceUp.setWidth("0px");
+		}
+
+		HorizontalPanel horizontalPanelSpaceText = new HorizontalPanel();
+		horizontalPanelSpaceText.setWidth("10px");
+		flexTable.setWidget(1, 1, horizontalPanelSpaceText);
+
+		HTML htmlMessage = new HTML(message, true);
+		flexTable.setWidget(1, 2, htmlMessage);
+
+		HorizontalPanel horizontalPanel = new HorizontalPanel();
+		horizontalPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+		horizontalPanel.setSize("100%", "100%");
+		HorizontalPanel innerPanel = new HorizontalPanel();
+		innerPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+		horizontalPanel.add(innerPanel);
+		flexTable.setWidget(2, 2, horizontalPanel);
+
+		button1.addClickHandler(new ClickHandler() 
+		{
+			public void onClick(ClickEvent event)
+			{
+				dBox.hide();
+				if (callback != null)
+					callback.complete(0);				
+			}
+		});
+		button2.addClickHandler(new ClickHandler() 
+		{
+			public void onClick(ClickEvent event)
+			{
+				dBox.hide();
+				if (callback != null)
+					callback.complete(1);				
+			}
+		});
+
+		buttonCancel = new ButtonImageText(ButtonImageText.Type.CANCEL,  lang._TextCancel());
+		
+		innerPanel.add(button1);
+		innerPanel.add(new HorizontalSpacer("1em"));
+		innerPanel.add(button2);
+		innerPanel.add(new HorizontalSpacer("10em"));
+		innerPanel.add(buttonCancel);
+
+		buttonCancel.addClickHandler(new ClickHandler() 
+		{
+			public void onClick(ClickEvent event)
+			{
+				dBox.hide();
+				if (callback != null)
+					callback.complete(-1);				
+			}
+		});
+		dBox.getElement().getStyle().setProperty("zIndex", "10" );
+		dBox.center();
+	}
+}
---
+++ test/com/TheJobCoach/webapp/util/client/TestMessageBoxTriState.java
@@ -0,0 +1,60 @@
+package com.TheJobCoach.webapp.util.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.googlecode.gwt.test.GwtModule;
+import com.googlecode.gwt.test.GwtTest;
+
+@GwtModule("com.TheJobCoach.webapp.userpage.UserPage")
+public class TestMessageBoxTriState extends GwtTest {
+
+	private MessageBoxTriState mb;
+
+	@Test
+	public void testMessageBox()
+	{
+
+	}
+
+	int accept;
+	
+	@Test
+	public void allTest()
+	{
+		HorizontalPanel p = new HorizontalPanel();
+		Button b1 = new Button();
+		Button b2 = new Button();
+		mb = new MessageBoxTriState(
+				p, MessageBoxTriState.TYPE.QUESTION, "title", "message", 
+				b1, b2, new MessageBoxTriState.ICallback() {
+
+					@Override
+					public void complete(int accepted) 
+					{
+						accept = accepted;
+					}
+				});
+		mb.onModuleLoad();		
+		assertTrue(mb.dBox.isShowing() == true);
+
+		b2.click();
+		assertTrue(1 == accept);
+		assertTrue(mb.dBox.isShowing() == false);
+		
+		mb.onModuleLoad();	
+		mb.buttonCancel.click();
+		assertTrue(-1 == accept);
+		assertTrue(mb.dBox.isShowing() == false);
+		
+		mb.onModuleLoad();	
+		b1.click();
+		assertTrue(0 == accept);
+		assertTrue(mb.dBox.isShowing() == false);
+		
+	}
+
+}
