1d933e1317514c7a50230d8b017a01a9ac656407
MathieuAvila <mathieu.avila@laposte.net>
2014-05-20 22:29:32 +0200

    log more information about loading of resource. This helps diagnose resource problems. Once loaded, message does not appear anymore

--- src/com/TheJobCoach/util/StringResourceCache.java
+++ src/com/TheJobCoach/util/StringResourceCache.java
@@ -5,18 +5,22 @@ import java.io.InputStream;
 import java.util.HashMap;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StringResourceCache {
 
 	static HashMap<String, String> cache = new HashMap<String, String>();
 	static private StringResourceCache localObj = new StringResourceCache();
-	
+	static Logger logger = LoggerFactory.getLogger(StringResourceCache.class);
+
 	static public String getStringResource(String key)
 	{
 		if (cache.containsKey(key))
 		{
 			return cache.get(key);
 		};
+		logger.info("Load file in cache from " + key);
 		InputStream contentFile = localObj.getClass().getResourceAsStream(key);
 		try
 		{
