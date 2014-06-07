b0b267bc1327ffad5754171cf71b6ade9f997fb1
MathieuAvila <mathieu.avila@laposte.net>
2014-06-07 22:36:56 +0200

    Indenting

--- src/com/TheJobCoach/webapp/util/shared/FormatUtil.java
+++ src/com/TheJobCoach/webapp/util/shared/FormatUtil.java
@@ -7,21 +7,21 @@ public class FormatUtil
 
 	public static String trueString = "1";
 	public static String falseString = "0";
-	
+
 	public enum PERIOD_TYPE 
 	{ 
 		PERIOD_TYPE_WEEK,
 		PERIOD_TYPE_2WEEKS,
 		PERIOD_TYPE_MONTH
 	};
-	
+
 	public static String fillWithZeroCount(int i, int nbDigit)
 	{
 		String v = String.valueOf(i);
 		while (v.length() < nbDigit) v = "0" + v;
 		return v;
 	}
-	
+
 	public static int[] readIntegersFromString(String s)
 	{
 		String[] st = s.split("_");
@@ -38,25 +38,25 @@ public class FormatUtil
 		}
 		return result;
 	}
-	
+
 	@SuppressWarnings({ "deprecation" })
 	public static String getDateString(Date d)
 	{
 		if (d == null) return "";
 		return fillWithZeroCount(d.getYear() + 1900 ,4) + "_" + 
-				fillWithZeroCount(d.getMonth(),2) + "_" +
-				fillWithZeroCount(d.getDate(), 2) + "_" +
-				fillWithZeroCount(d.getHours(),2) + "_" +
-				fillWithZeroCount(d.getMinutes(),2) + "_" +
-				fillWithZeroCount(d.getSeconds(),2);
+		fillWithZeroCount(d.getMonth(),2) + "_" +
+		fillWithZeroCount(d.getDate(), 2) + "_" +
+		fillWithZeroCount(d.getHours(),2) + "_" +
+		fillWithZeroCount(d.getMinutes(),2) + "_" +
+		fillWithZeroCount(d.getSeconds(),2);
 	}
-	
+
 	static int getIntFromIndex(int[] table, int index)
 	{
 		if (index < table.length) return table[index];
 		return 0;
 	}
-	
+
 	@SuppressWarnings({ "deprecation" })
 	public static Date getStringDate(String s)
 	{
@@ -71,7 +71,7 @@ public class FormatUtil
 		d.setSeconds(getIntFromIndex(table, 5));
 		return d;
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	public static Date startOfTheDay(Date d)
 	{
@@ -82,7 +82,7 @@ public class FormatUtil
 		r.setHours(0);
 		return r;
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	public static Date sameDayTimeAs(Date d)
 	{
@@ -102,7 +102,7 @@ public class FormatUtil
 		r.setSeconds(59);
 		return r;
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	public static Date startOfTheUniverse()
 	{
@@ -120,7 +120,7 @@ public class FormatUtil
 
 	public void copyDate()
 	{
-		
+
 	}
 
 	@SuppressWarnings("deprecation")
@@ -150,7 +150,7 @@ public class FormatUtil
 			long diffWeek = (currentRef - fixedRef) / MS_PER_WEEK + ((currentRef - fixedRef) < 0 ? -1*interval : 0);
 
 			long boundary = diffWeek % interval;
-			
+
 			long startWeekMs = fixedRef + diffWeek * MS_PER_WEEK;
 			long startPeriodMs = startWeekMs + (count * interval - boundary ) * MS_PER_WEEK;
 			long endPeriodMs = startPeriodMs + interval * MS_PER_WEEK;
@@ -161,6 +161,15 @@ public class FormatUtil
 		default:
 			break;
 		}
-		
+	}
+
+	static public Integer getIntegerFromString(String v)
+	{
+		Integer r = null;
+		if (v == null) return null;
+		try {
+			r = Integer.parseInt(v);
+		} catch (Exception e)	{}
+		return r;			
 	}
 }
