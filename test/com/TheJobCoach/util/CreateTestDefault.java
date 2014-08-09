4b6449f09dbd0d0ffbe512f1c9fe005e602abbbb
MathieuAvila <mathieu.avila@laposte.net>
2014-08-09 17:43:48 +0200

    fill user1 with lot of data for connection tests

--- test/com/TheJobCoach/util/CreateTestDefault.java
+++ test/com/TheJobCoach/util/CreateTestDefault.java
@@ -2,11 +2,31 @@ package com.TheJobCoach.util;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Vector;
+
+import com.TheJobCoach.CoachTestUtils;
 import com.TheJobCoach.userdata.AccountManager;
 import com.TheJobCoach.userdata.ContactManager;
+import com.TheJobCoach.userdata.UserDocumentManager;
+import com.TheJobCoach.userdata.UserExternalContactManager;
+import com.TheJobCoach.userdata.UserLogManager;
+import com.TheJobCoach.userdata.UserOpportunityManager;
 import com.TheJobCoach.webapp.mainpage.shared.UserInformation;
 import com.TheJobCoach.webapp.userpage.shared.ContactInformation;
+import com.TheJobCoach.webapp.userpage.shared.ExternalContact;
+import com.TheJobCoach.webapp.userpage.shared.UpdatePeriod;
+import com.TheJobCoach.webapp.userpage.shared.UserDocument;
+import com.TheJobCoach.webapp.userpage.shared.UserDocumentId;
+import com.TheJobCoach.webapp.userpage.shared.UserDocumentRevision;
+import com.TheJobCoach.webapp.userpage.shared.UserLogEntry;
+import com.TheJobCoach.webapp.userpage.shared.UserOpportunity;
+import com.TheJobCoach.webapp.userpage.shared.ContactInformation.Visibility;
+import com.TheJobCoach.webapp.userpage.shared.UpdatePeriod.PeriodType;
+import com.TheJobCoach.webapp.userpage.shared.UserLogEntry.LogEntryType;
 import com.TheJobCoach.webapp.util.shared.CassandraException;
+import com.TheJobCoach.webapp.util.shared.SystemException;
 import com.TheJobCoach.webapp.util.shared.UserId;
 
 import org.junit.Test;
@@ -23,19 +43,20 @@ public class CreateTestDefault {
 		String userMail = account.getUsernameFromEmail(email);
 		try { 
 			account.deleteAccount(userName);
-		account.deleteAccount(userMail);} 
+			account.deleteAccount(userMail);} 
 		catch (Exception e) {};
 	}
-	
+
 	@Test
-	public void testCreateAccount() throws CassandraException, InterruptedException
+	public void testCreateAccount() throws CassandraException, InterruptedException, SystemException
 	{
 		MockMailer mockMail = new MockMailer();
 		MailerFactory.setMailer(mockMail);
-		
+
 		UserId user1 = new UserId("user1","mytoken1", UserId.UserType.USER_TYPE_SEEKER);
 		UserId user2 = new UserId("user2","mytoken2", UserId.UserType.USER_TYPE_SEEKER);
-		
+		UserId user3 = new UserId("user3","mytoken3", UserId.UserType.USER_TYPE_SEEKER);
+
 		{
 			UserInformation userInfo = new UserInformation("Nuser1", "mathieu.avila@laposte.net", "password", "Nprenomuser1");
 			deleteAccountNoException("user1", userInfo.email);
@@ -59,13 +80,133 @@ public class CreateTestDefault {
 		}
 		Thread.sleep(1000);
 		{
+			UserInformation userInfo = new UserInformation("Nuser3", "mathieu.avila@google.com", "password", "Nprenomuser3");
+			deleteAccountNoException("user3", userInfo.email);
+			logger.info("create account " + account.createAccountWithToken(
+					user3,
+					userInfo, "en"));
+			account.validateAccount(user3.userName, user3.token);
+			account.loginAccount(user3.userName, "password");
+			logger.info("logging in user3: " + account.loginAccount("user3", "password").getLoginStatus());
+		}
+		Thread.sleep(1000);
+		{
 			ContactManager contactManager1 = new ContactManager(user1);
 			ContactManager contactManager2 = new ContactManager(user2);
+			ContactManager contactManager3 = new ContactManager(user3);
 			// connect user1 and user2
 			ContactInformation.ContactStatus newStatus = contactManager1.updateContactRequest(user2, true);
 			assertEquals(ContactInformation.ContactStatus.CONTACT_REQUESTED, newStatus);
 			newStatus = contactManager2.updateContactRequest(user1, true);
 			assertEquals(ContactInformation.ContactStatus.CONTACT_OK, newStatus);
+			// change 1 => 2 clearance
+			Visibility v_OLCD = new Visibility(true,true,true,true);
+			contactManager1.setUserClearance(user2.userName, v_OLCD);
+
+			// connect user1 and user3
+			newStatus = contactManager1.updateContactRequest(user3, true);
+			assertEquals(ContactInformation.ContactStatus.CONTACT_REQUESTED, newStatus);
+			newStatus = contactManager3.updateContactRequest(user1, true);
+			assertEquals(ContactInformation.ContactStatus.CONTACT_OK, newStatus);
+			// connect user2 and user3
+			newStatus = contactManager2.updateContactRequest(user3, true);
+			assertEquals(ContactInformation.ContactStatus.CONTACT_REQUESTED, newStatus);
+			newStatus = contactManager3.updateContactRequest(user2, true);
+			assertEquals(ContactInformation.ContactStatus.CONTACT_OK, newStatus);
+
+			// change 3 => 2 clearance
+			Visibility v_olCD = new Visibility(true,true,false,false);
+			contactManager3.setUserClearance(user2.userName, v_olCD);			
+		}
+		{
+
+			// set documents
+			UserDocumentManager docManager = UserDocumentManager.getInstance();
+
+			String ud1_id = "doc1";
+			String ud2_id = "doc2";
+			String ud3_id = "doc3";
+			byte[] c1 = { 0, 1, 2};
+			byte[] c2 = { 3, 4, 5};
+			byte[] c21 = { 6, 7, 8};
+			
+			UserDocumentRevision rev1 = new UserDocumentRevision(CoachTestUtils.getDate(2000, 12, 1), ud1_id, "file1");
+			UserDocument ud1 = new UserDocument(
+					ud1_id, "ndoc1", "description1", CoachTestUtils.getDate(2000, 12, 1), "file1", 
+					UserDocument.DocumentStatus.NEW, UserDocument.DocumentType.RESUME, new Vector<UserDocumentRevision>(Arrays.asList(rev1)));
+
+			UserDocumentRevision rev2 = new UserDocumentRevision(CoachTestUtils.getDate(2000, 12, 2), ud2_id, "file2");
+			UserDocument ud2 = new UserDocument(
+					ud2_id, "ndoc2", "description2", CoachTestUtils.getDate(2000, 12, 2), "file2", 
+					UserDocument.DocumentStatus.OUTDATED, UserDocument.DocumentType.MOTIVATION, new Vector<UserDocumentRevision>(Arrays.asList(rev2)));
+
+			UserDocumentRevision rev3 = new UserDocumentRevision(CoachTestUtils.getDate(2000, 12, 3), ud3_id, "file3");
+			UserDocument ud3 = new UserDocument(
+					ud3_id, "ndoc1", "description1", CoachTestUtils.getDate(2000, 12, 3), "file3", 
+					UserDocument.DocumentStatus.SECONDARY, UserDocument.DocumentType.OTHER, new Vector<UserDocumentRevision>(Arrays.asList(rev3)));
+			
+			docManager.setUserDocument(user1, ud1);
+			docManager.setUserDocument(user1, ud2);
+			docManager.setUserDocument(user1, ud3);
+			
+			docManager.setUserDocumentContent(user1, ud1_id, "f1", c1);
+			docManager.setUserDocumentContent(user1, ud2_id, "f2", c2);
+			docManager.setUserDocumentContent(user1, ud3_id, "f21", c21);
+	
+			// User 1 opportunities / logs
+			UserOpportunityManager managerOpportunity1 = new UserOpportunityManager();
+			UserOpportunity opportunity1_1 = new UserOpportunity("opp1", CoachTestUtils.getDate(2000, 1, 1), CoachTestUtils.getDate(2000, 2, 1),
+					"title1", "description1", "companyId1",
+					"contractType1",  "1",  
+					CoachTestUtils.getDate(2000, 1, 1), CoachTestUtils.getDate(2000, 1, 1),
+					false, "source1", "url1", "location1",
+					UserOpportunity.ApplicationStatus.APPLIED, "note1");
+
+			UserOpportunity opportunity1_2 = new UserOpportunity("opp2", CoachTestUtils.getDate(2000, 1, 2), CoachTestUtils.getDate(2000, 2, 2),
+					"title2", "description2", "companyId2",
+					"contractType2",  "2",  
+					CoachTestUtils.getDate(2000, 1, 2), CoachTestUtils.getDate(2000, 1, 2),
+					false, "source2", "url2", "location2",
+					UserOpportunity.ApplicationStatus.NEW, "note2");
+			managerOpportunity1.setUserOpportunity(user1, opportunity1_1, "managed");
+			managerOpportunity1.setUserOpportunity(user1, opportunity1_2, "managed");
+
+			// set associated logs
+			Vector<ExternalContact> externalContactList_void = new Vector<ExternalContact>();
+			UserDocumentId docId1_1 = new UserDocumentId("id1", "id1", "name1", "fileName1", new Date(), new Date());
+			UserDocumentId docId1_2 = new UserDocumentId("id2", "id2", "name2", "fileName2", new Date(), new Date());
+			UserDocumentId docId1_3 = new UserDocumentId("id3", "id3", "name3", "fileName3", new Date(), new Date());
+			Vector<UserDocumentId> docIdList = new Vector<UserDocumentId>(Arrays.asList(docId1_1, docId1_2, docId1_3));
+			Vector<UserDocumentId> docIdListVoid = new Vector<UserDocumentId>();
+
+			UserLogEntry userLog1_1 = new UserLogEntry("opp1", "log1", "title1", "description1", 
+					CoachTestUtils.getDate(2000, 2, 1),
+					LogEntryType.INFO, externalContactList_void, docIdList, "note1", false);
+
+			UserLogEntry userLog1_2 = new UserLogEntry("opp1", "log2", "title1", "description1", 
+					CoachTestUtils.getDate(2000, 2, 1),
+					LogEntryType.INFO, externalContactList_void, docIdListVoid, "note1_less", false);
+
+			UserLogEntry userLog1_3 = new UserLogEntry("opp1", "log3", "title3", "description3", 
+					CoachTestUtils.getDate(2000, 2, 1),
+					LogEntryType.INFO, externalContactList_void, docIdListVoid, "note2", false);
+			UserLogManager logManager1 = new UserLogManager();
+			logManager1.setUserLogEntry(user1, userLog1_1);
+			logManager1.setUserLogEntry(user1, userLog1_2);
+			logManager1.setUserLogEntry(user1, userLog1_3);
+
+			// set external contact
+			String contact1 = "contact1";
+			String contact2 = "contact2";
+			String contact3 = "contact3";
+			ExternalContact external_contact1 = new ExternalContact(contact1, "firstName1", "lastName1", "email1", "phone1", "personalNote1", "organization1", new UpdatePeriod(CoachTestUtils.getDate(2000, 1, 1), 2, PeriodType.DAY, true));
+			ExternalContact external_contact2 = new ExternalContact(contact2, "firstName2", "lastName2", "email2", "phone2", "personalNote2", "organization2", new UpdatePeriod(CoachTestUtils.getDate(2000, 1, 1), 2, PeriodType.DAY, false));
+			ExternalContact external_contact3 = new ExternalContact(contact3, "firstName3", "lastName3", "email3", "phone3", "personalNote3", "organization3", new UpdatePeriod(CoachTestUtils.getDate(2000, 1, 1), 2, PeriodType.DAY, false));
+
+			UserExternalContactManager contactManager1 = new UserExternalContactManager();
+			contactManager1.setExternalContact(user1, external_contact1);
+			contactManager1.setExternalContact(user1, external_contact2);
+			contactManager1.setExternalContact(user1, external_contact3);
 		}
 		Thread.sleep(1000);
 		{
